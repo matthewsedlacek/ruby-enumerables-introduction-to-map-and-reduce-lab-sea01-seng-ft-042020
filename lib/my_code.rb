eac54741cc0babd56f920d8d78e2131d3d52e726
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-28 21:31:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,9 +2,11 @@
 
 def map_to_negativize(source_array)
   i = 0 
+  result = []
   
   while i < source_array.length do
-  source_array.length[i] *-1
+  source_array.length[i]
+  result.push(source_array.length[i] *-1)
   i +=1
   end
 
