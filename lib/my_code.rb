1d70eab643b50c25838910a2f4f23c9e0a91e890
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-28 21:58:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,7 +51,7 @@ end
 def reduce_to_total(source_array,starting_point)
   i = 0
   
-  if starting_point = nil total = starting_poin
+  if starting_point = nil total = 0 
   else 
     total = starting_point
  
