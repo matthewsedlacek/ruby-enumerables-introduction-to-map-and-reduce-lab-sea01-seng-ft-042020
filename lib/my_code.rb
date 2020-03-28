ecbe100123a6910488b6616d99b93a5e2e1b431d
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-28 21:44:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -46,3 +46,15 @@ def map_to_square(source_array)
   end
   result
 end
+
+
+def reduce_to_total(source_array,starting_point)
+  i = 0 
+  result = []
+  
+  while i < source_array.length do
+  result << (source_array[i] *-1)
+  i +=1
+  end
+  result
+end
