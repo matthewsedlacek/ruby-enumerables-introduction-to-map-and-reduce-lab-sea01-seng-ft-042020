3825d1ae30e7a192cdb64dab8e878a5ae0a8870e
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-28 21:42:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -22,12 +22,12 @@ def map_to_no_change(source_array)
   result
 end
 
-def map_to_negativize(source_array)
+def map_to_double(source_array)
   i = 0 
   result = []
   
   while i < source_array.length do
-  result << (source_array[i] *-1)
+  result << (source_array[i] *2)
   i +=1
   end
   result
